f594989ac6dfcc71c2a25518cf07f7a53fdf62b1
kaiccesar1 <kaic_cesar@hotmail.com>
2026-08-13 20:15:19 -0300

    feat: cadastrar livro

---
+++ src/entities/Livro.java
@@ -0,0 +1,51 @@
+package entities;
+
+public class Livro {
+    private int id;
+    private String titulo;
+    private String autor;
+    private boolean disponivel;
+
+    public Livro(int id, String titulo, String autor, boolean disponivel){
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.disponivel = disponivel;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+    @Override
+    public String toString(){
+        return  "ID: " + id
+                + ", Titulo: " + titulo
+                + ", Autor: " + autor
+                + ", Disponivel: " + disponivel;
+    }
+}
