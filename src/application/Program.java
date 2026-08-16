3a03340d111175445ada3209d68ce4f7b6748446
kaiccesar1 <kaic_cesar@hotmail.com>
2026-08-16 19:31:24 -0300

    feat: buscar livro pelo ID

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import entities.Biblioteca;
@@ -12,29 +13,43 @@ public class Program {
 
         int opcao = 1;
 
-        while(opcao != 0){
 
-            System.out.println("===== Library Manager =====");
-            System.out.println("1 - Cadastrar livro");
-            System.out.println("2 - Lista livros");
-            System.out.println("0 - Sair");
+        try {
+            while(opcao != 0) {
 
-            System.out.print("Escolha uma opção: ");
-            opcao = sc.nextInt();
-            sc.nextLine();
-            switch (opcao) {
-                case 1:
-                    System.out.print("Titulo: ");
-                    String titulo = sc.nextLine();
+                System.out.println("===== Library Manager =====");
+                System.out.println("1 - Cadastrar livro");
+                System.out.println("2 - Lista livros");
+                System.out.println("3 - Buscar livro");
+                System.out.println("0 - Sair");
 
-                    System.out.print("Autor: ");
-                    String autor  = sc.nextLine();
+                System.out.print("Escolha uma opção: ");
+                opcao = sc.nextInt();
+                sc.nextLine();
+                switch (opcao) {
+                    case 1:
+                        System.out.print("Titulo: ");
+                        String titulo = sc.nextLine();
 
-                    biblioteca.cadastrarLivro(titulo, autor);
-                    break;
-                case 2:
-                    biblioteca.listarLivros();
+                        System.out.print("Autor: ");
+                        String autor = sc.nextLine();
+
+                        biblioteca.cadastrarLivro(titulo, autor);
+                        break;
+                    case 2:
+                        biblioteca.listarLivros();
+                        break;
+                    case 3:
+                        System.out.print("Informe o ID do livro: ");
+                        int id = sc.nextInt();
+                        sc.nextLine();
+
+                        biblioteca.listarLivro(id);
+                        break;
+                }
             }
+        } catch (InputMismatchException e){
+            System.out.println("Descrição do erro: " + e.getMessage());
         }
     }
 }
