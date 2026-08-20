6cd15ac847463dda7885d7608a70af7bdfb8d25b
kaiccesar1 <kaic_cesar@hotmail.com>
2026-08-20 12:04:57 -0300

    implementar cadastro e listagem de usuarios

--- src/entities/Usuario.java
+++ src/entities/Usuario.java
@@ -1,4 +1,61 @@
 package entities;
 
 public class Usuario {
+    private int id;
+    private String name;
+    private String cpf;
+    private String email;
+
+    public Usuario(){
+
+    }
+
+    public Usuario(int id, String name, String cpf, String email) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
--- src/entities/UsuarioService.java
+++ src/entities/UsuarioService.java
@@ -1,4 +1,19 @@
 package entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UsuarioService {
+    List<Usuario> list = new ArrayList<>();
+
+    public void cadastrarUsuario(Usuario user){
+        list.add(user);
+        System.out.println("Cadastrado com sucesso!");
+    }
+
+    public void listarUsuarios(){
+        for(Usuario user : list){
+            System.out.println(user);
+        }
+    }
 }
